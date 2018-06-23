b1eb2bbec8eefa109fd155bb34fdfbfbe14f67e8
Mehret Kulala <merutaye@gmail.com>
2018-06-23 13:32:52 -0500

    adding projects to firebase complete

---
+++ javascripts/projects/projectsFirebaseApi.js
@@ -0,0 +1,31 @@
+const {getSetFirebaseConfig,} = require('../apiKeys');
+
+let firebaseConfig = {};
+
+const getAllProjects = () => {
+  firebaseConfig = getSetFirebaseConfig();
+  console.log('getAllProjects:', firebaseConfig);
+  return new Promise((resolve, reject) => {
+    const allProjectsArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/projects.json`,
+    })
+      .done((allProjectsObj) => {
+        if (allProjectsObj !== null) {
+          Object.keys(allProjectsObj).forEach((fbKey) => {
+            allProjectsObj[fbKey].id = fbKey;
+            allProjectsArray.push(allProjectsObj[fbKey]);
+          });
+        }
+        resolve(allProjectsArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = {
+  getAllProjects,
+};
