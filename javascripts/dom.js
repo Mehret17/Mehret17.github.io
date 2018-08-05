aea1a41efea42ce44ad9c4b4e4f753711f456fb3
Mehret Kulala <merutaye@gmail.com>
2018-08-05 10:41:57 -0500

    changes made wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -4,16 +4,16 @@ const projectDiv = $('#myProjects');
 const createProjectsCard = (projectArray) => {
   let projectString = '';
   projectArray.forEach((projects) => {
-    projectString += `<div class="container-fluid>`;
+    projectString += `<div class="container-fluid">`;
     projectString += `<div class="row">`;
-    projectString += `<div class="col-sm-6">`;
+    projectString += `<div class="col-md-6 col-md-3-offset">`;
     projectString += `<div class='thumbnail projectsCard'>`;
     projectString += `<img src="${projects.thumbnail}"</h3>`;
     projectString += `<div class="caption">`;
     projectString += `<p class="projectDescription">${projects.description}</p>`;
     projectString += `<p><a id="github"href="${projects.github}" class="btn btn-primary text-center">View on Github</a></p>`;
     projectString += `</div>`;
-    projectString += `</div>`;
+    // projectString += `</div>`;
     projectString += `</div>`;
     projectString += `</div>`;
   });
@@ -29,8 +29,8 @@ const outPutDiv = $('#myBlogs');
 const createBlogPosts = (blogsArray) => {
   let domString = '';
   blogsArray.forEach((blogs) => {
-    domString += `<div class="container-fluid>"`;
-    domString += `<div class= "col-sm-8 col-md-6">`;
+    domString += `<div class="container-fluid col-md-6 col-md-offset-3">`;
+    domString += `<div class= "col-md-6 col-md-offset-3">`;
     domString += `<div class = "blogPost">`;
     domString += `<p class ="blogDate">${blogs.date}</p>`;
     domString += `<h3 class="blogTitle">${blogs.title}</h3>`;
